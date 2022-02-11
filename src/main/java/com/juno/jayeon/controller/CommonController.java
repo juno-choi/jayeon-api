df0ae99431297afe0073ce61bc5b2e84132660d7
juno-choi <ililil9482@naver.com>
2022-02-11 15:24:45 +0900

    FEAT : 실서버 docs 나오지 않는 오류 수정을 위한 thymeleaf 추가

--- src/main/java/com/juno/jayeon/controller/CommonController.java
+++ src/main/java/com/juno/jayeon/controller/CommonController.java
@@ -3,12 +3,14 @@ package com.juno.jayeon.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/v1/docs")
 public class CommonController {
     @GetMapping("")
     public String docs(){
-        return "/docs/api.html";
+        return "docs";
     }
 }
--- src/test/java/com/juno/jayeon/controller/ItemControllerTest.java
+++ src/test/java/com/juno/jayeon/controller/ItemControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
